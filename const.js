a6a48741ea0bf968984f314bf2e44e5edb9ec2e5
刘成 <lttsd@foxmail.com>
2019-06-23 15:52:41 +0800

    添加环境变量设置

--- const.js
+++ const.js
@@ -1,5 +1,16 @@
-const resUrl = "http://192.168.1.6:8082";
-const mp3FilePath = "/Users/lttsd/Desktop/resource/mp3";
+const env = require("./env");
+
+let resUrl;
+let mp3FilePath;
+if(env === "dev") {
+    resUrl = "http://192.168.1.6:8082";
+    mp3FilePath = "/Users/lttsd/Desktop/resource/mp3";
+}else if (env === "prod") {
+    resUrl = "http://118.31.224.27";
+    mp3FilePath = "/root/nginx/upload/mp3";
+
+}
+
 
 const category = [
     'Biomedicine',
